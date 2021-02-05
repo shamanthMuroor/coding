2d393314d15cea91997deb527b215c8b014de9ff
shamanthMuroor <shamanthm3@gmail.com>
2021-02-05 23:38:19 +0530

    String Task

---
+++ CodeForces/118a-StringTask.py
@@ -0,0 +1,11 @@
+#import string
+
+str = input().lower()
+vowel = ['a', 'e', 'i', 'o', 'u', 'y'] #y is also included in the question for some reason
+res = ""
+
+for letter in str:
+    if letter not in vowel:
+        res = res + "." + letter
+
+print(res)
