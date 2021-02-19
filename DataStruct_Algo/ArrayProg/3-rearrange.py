8149c34db7f7973241ccca310d0a5de5761cd051
shamanthMuroor <shamanthm3@gmail.com>
2021-02-20 00:31:46 +0530

    updated code

--- DataStruct_Algo/ArrayProg/3-rearrange.py
+++ DataStruct_Algo/ArrayProg/3-rearrange.py
@@ -1,12 +1,22 @@
-ar = [ -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 ]
+# Given an array of elements of length N, ranging from 0 to N – 1. 
+# Rearrange the array such that A[i] = i and if i is not present, 
+# display -1 at that place.
 
-ar = list(filter(lambda n: n!=-1, ar))
-ar = ar.sort()
-print(type(ar))
-for i in range(len(ar)):
-    if ar[i] != i:
-        new_ar = -1
-    else:
-        new_ar = i
+import random
 
-print(new_ar)
+def RearrangeArray(ar):
+    new_ar = []
+    for i in range(len(ar)):
+        if i in ar:
+            new_ar.append(i)
+        else:
+            new_ar.append(-1)
+    print(new_ar)
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(random.randrange(0,10))
+#arr = [ -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 ]
+#arr = [ 19, 7, 0, 3, 18, 15, 12, 6, 1, 8, 11, 10, 9, 5, 13, 16, 2, 14, 17, 4 ]
+RearrangeArray(arr)
