fec45b1b4bb92f0fd610a6c16a00d4c6e60c2984
shamanthMuroor <shamanthm3@gmail.com>
2021-02-23 00:02:13 +0530

    map operations updated

--- Cpp/LearningCpp/STL_examples/sets_maps.cpp
+++ Cpp/LearningCpp/STL_examples/sets_maps.cpp
@@ -28,5 +28,26 @@ int main() {
     cout << *s.begin() << "\n";
     auto it = s.end();
     cout <<  *(--it) << endl;
+
+    // Unorderd Map
+    unordered_map<int, int> um;
+    um[1] = 5;
+    um[3] = 14;
+    um[2] = 7;
+    um.erase(2);
+    cout << um[1] << endl;
+    cout << um.count(7) << endl;
+    cout << um.count(1) << endl; 
+
+    // Ordered Map
+    map<int, int> m;
+    m[3] = 6;
+    m[11] = 11;
+    m[10] = 491;
+    cout << m.lower_bound(10)->first << " " << m.lower_bound(10) -> second << endl;
+    cout << m.upper_bound(10)->first << " " << m.upper_bound(10) -> second << endl;
+    m.erase(11);
+    if(m.upper_bound(10) == m.end())
+        cout << "End" << endl;
     return 0;
 }
