9d67102b976ca41a1ef7ee4efed9ab5b18e354bf
shamanthMuroor <shamanthm3@gmail.com>
2021-04-25 20:21:07 +0530

    time limit exceeded

--- CodeForces/CppCodeForces/1496b-MaxMex.cpp
+++ CodeForces/CppCodeForces/1496b-MaxMex.cpp
@@ -1,8 +1,8 @@
-// Time limit exceeded
-
+// // Time limit exceeded
 
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <cmath>
 
@@ -12,94 +12,114 @@ int main() {
     int t;
     cin >> t; 
     while(t--) {
-        long long int n, k, val, mini, b, i, a, ele;    
-        vector<long long int> arr;
+        bool flag = false;
+        long long int n, k, val, mini, i, MINI, MAXI=0;    
+        set<long long int> s;
         vector<long long int> temp;
         cin >> n >> k;
         for(int i=0; i<n; i++) {
             cin >> val;
-            arr.push_back(val);
+            s.insert(val);
+            MAXI = max(MAXI, val);
+            temp.push_back(val);
         }
 
         while(k--) {
             // a = mex(s)
-            temp = arr;
-            for(i=0; i<1000000000; i++) {
-                sort(temp.begin(), temp.end());
+            sort(temp.begin(), temp.end());
+            for(i=0; i<n+1; i++) {
                 mini = *min_element(temp.begin(), temp.end());
                 if(min(i, mini) == mini) {
                     if(mini == temp.back()) {
-                        a = mini + 1;
+                        MINI = mini + 1;
+                        flag = true;
                         break;
                     }
                     vector<long long int>::iterator iter = find(temp.begin(), temp.end(), mini);
                     temp.erase(iter);
                 }
                 else {
-                    a = i;
+                    MINI = i;
                     break;
                 }
             }
 
-            // b = max(s)            
-            b = *max_element(arr.begin(), arr.end());
             // (a+b)/2
-            ele = ceil((float)(a+b)/2);
-            arr.push_back(ele);
+            s.insert(ceil((float)(MINI+MAXI)/2));
+            if(flag) 
+                break;
         }
-            sort(arr.begin(), arr.end());
-            auto last = unique(arr.begin(), arr.end());
-            arr.erase(last, arr.end());
-            cout << arr.size() << endl;
+
+            if(flag) {
+                cout << s.size() + k << endl;
+            }
+            else
+                cout << s.size() << endl;
     }
     return 0;
 }
 
 
+// // // Time limit exceeded
 
-
-
-
-
-
-
-
-
-
-// // NOT SOLVED
-
-// #include <bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <cmath>
 
 // using namespace std;
 
-// #define mx 10e9
-
-// void solve() {
-//     int n, k, x;
-//     set<int> s;
-//     cin >> n >> k;
-//     for(int i=0; i<n; ++i) {
-//         cin >> x;
-//         s.insert(x);
-//     }
-//     for(int i=0; i<k; ++i) {
-//         int mn = 0;
-//         for(int element : s) {
-//             if(element - mn == 0)
-//                 ++mn;
-//             else   
-//                 break;
-//         }
-//         s.insert((mn + *(s.end()))/2);
-//     }
-//     cout << s.size() << endl;
-// }
-
 // int main() {
 //     int t;
-//     cin >> t;
-//     for(int i=0; i<t; i++) {
-//         solve();
+//     cin >> t; 
+//     while(t--) {
+//         bool flag = false;
+//         long long int n, k, val, mini, b, i, a, ele;    
+//         vector<long long int> s;
+//         vector<long long int> temp;
+//         cin >> n >> k;
+//         for(int i=0; i<n; i++) {
+//             cin >> val;
+//             s.push_back(val);
+//         }
+
+//         while(k--) {
+//             // a = mex(s)
+//             temp = s;
+//             sort(temp.begin(), temp.end());
+//             for(i=0; i<1000000000; i++) {
+//                 mini = *min_element(temp.begin(), temp.end());
+//                 if(min(i, mini) == mini) {
+//                     if(mini == temp.back()) {
+//                         a = mini + 1;
+//                         flag = true;
+//                         // cout << "Extra: " << extra << endl;
+//                         break;
+//                     }
+//                     vector<long long int>::iterator iter = find(temp.begin(), temp.end(), mini);
+//                     temp.erase(iter);
+//                 }
+//                 else {
+//                     a = i;
+//                     break;
+//                 }
+//             }
+
+//             // b = max(s)            
+//             b = *max_element(s.begin(), s.end());
+//             // (a+b)/2
+//             s.push_back(ceil((float)(a+b)/2));
+//             if(flag) 
+//                 break;
+//         }
+//             sort(s.begin(), s.end());
+//             auto last = unique(s.begin(), s.end());
+//             s.erase(last, s.end());
+//             if(flag) {
+//                 cout << s.size() + k << endl;
+//             }
+//             else
+//                 cout << s.size() << endl;
 //     }
 //     return 0;
 // }
