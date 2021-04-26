6ebbad95478050996ef9cfb6de8927dbd04dc8c4
shamanthMuroor <shamanthm3@gmail.com>
2021-04-26 17:01:09 +0530

    vector usage - easy

--- CodeForces/CppCodeForces/1367b-EvenArray.cpp
+++ CodeForces/CppCodeForces/1367b-EvenArray.cpp
@@ -1,29 +1,33 @@
+// 114273159	Apr/26/2021 16:58UTC+5.5	shamanthmuroor	1367B - Even Array	GNU C++17	Accepted	30 ms	200 KB
+
+// 1367 B - Even Array
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int t, n, inp, wrongOdd=0, wrongEven=0; 
+    int t;
     scanf("%d", &t);
     while(t--) {
+        int n, inp, wrongOdd=0, wrongEven=0; 
         vector<int> arr;
         scanf("%d", &n);
         for(int i=0; i<n; i++) {
             scanf("%d", &inp);
             arr.push_back(inp);
             if( i%2 == 0 ) {
-                cout << i << " " ;
-                if(inp % 2 != 0)
-                    cout << inp;
+                if(inp % 2 != 0) {
                     wrongOdd++;
+                }
             }
-            else
-                if(inp % 2 == 0)
+            else {
+                if(inp % 2 == 0) {
                     wrongEven++;
+                }
+            }
         }
-        if(wrongOdd == 0 && wrongEven ==0)
-            printf("0\n");
-        else if(wrongOdd == wrongEven && wrongOdd != 0 && wrongEven !=0 ) 
+        if(wrongOdd == wrongEven) 
             printf("%d\n", wrongOdd);
         else   
             printf("-1\n");
