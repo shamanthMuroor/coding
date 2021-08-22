5e3c7e26c11e66bf0da0eef6eaf1e7eeda2fd1b5
shamanthMuroor <shamanthm3@gmail.com>
2021-08-22 18:01:58 +0530

    basic solution

---
+++ CodeForces/CppCodeForces/1339a-FillingDiamonds.cpp
@@ -0,0 +1,15 @@
+// 126688119	Aug/22/2021 17:47UTC+5.5	shamanthmuroor	1339A - Filling Diamonds	GNU C++17	Accepted	78 ms	3700 KB
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << n << endl;
+    }
+}
